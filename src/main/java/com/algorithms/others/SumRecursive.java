1780a620b29a5df3b198a7a745c9af0f4a512cb0
Davies Mugume A <davies.mugume@gmail.com>
2018-01-18 15:57:44 +0300

    Update SumRecursive.java

--- src/main/java/com/algorithms/others/SumRecursive.java
+++ src/main/java/com/algorithms/others/SumRecursive.java
@@ -10,35 +10,14 @@ public class SumRecursive {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         int array[] = {1, 2, 3, 4, 5, 10};
         int arrayLength = array.length;
         
-
-        System.out.println("Sum: " + sumArraySG(array, 0, 0, arrayLength));
-        System.out.println("Sum: " + sumArrayG4G(array, arrayLength));
-        System.out.println("Sum: " + sumArrayG4GModified(array, 0, arrayLength));
-
+        System.out.println("Sum: " + sumArray(array, 0, 0, arrayLength));
     }
 
-    //Sum (1,2,5,3,9) = sum(1,2,5,3) + sum(9) -> but we know that the sum (n) == n i.e. the sum(9) == 9
-    //so break-down problem recursively till the problem we know to solve
-    static int sumArrayG4G(int array[], int arrayLength) {
-
-        if (arrayLength <= 0) {
-            return 0;
-        }
-        return sumArrayG4G(array, arrayLength - 1) + array[arrayLength - 1];
-    }
-
-    static int sumArrayG4GModified(int array[], int index, int arrayLength) {
-
-        if (index >= arrayLength) {
-            return 0;
-        }
-        return array[index] + sumArrayG4GModified(array, index + 1, arrayLength);
-    }
-
-    static int sumArraySG(int[] array, int index, int sum, int arrayLength) {
+    static int sumArray(int[] array, int index, int sum, int arrayLength) {
 
         if (index >= arrayLength) {
             return sum;
